97cfe6ba4ab1bdf2c4b4372b5201ff753d8b27cd
Empya1 <161871238+Empya1@users.noreply.github.com>
2024-06-06 22:27:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+app.config["SECRET_KEY"] = "SECRET KEY"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bla.sqlite3"
 
 
@@ -29,6 +29,19 @@ def index():
 
 def save_img():
 	
-	data = request.json
+	data = request.json()
+	
+	with open(os.path.join("a.jpg"), "wb") as p:
+		p.write(data["image"])
 	
 	return jsonify(data)
+
+@app.route("/view")
+
+def viewimg():
+	print(os.path.join("a.jpg"))
+		
+	return render_template("img.html", url=str(os.path.join("a.jpg")))
+
+"""if __name__ == "__main__":	
+	app.run()"""
